b2969b343527b28d96a596220df30805d0a81ce9
dmlukichev <dmlukichev@gmail.com>
2015-03-06 21:02:59 +0300

    [dev] Build system configured

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ wagwan
 ======
 
 Wagwan web application
+
+### Installation
+```bash
+npm install
+gulp install
+```
+
+### Development
+
+
+To automatically recompile sources during development run:
+```bash
+gulp watch
+```
+
+Gulp tasks available:
+
+ name | description
+ --- | ---
+js | Compile JS
+js-app | Concat and minify all wagman's scripts
+js-vendor | Concat and minify 3rd parties scripts
+js-ie8 | Concat ie8 polyfills
+less | Compile LESS
+less-bootstrap | Compile bootstrap
+less-bootstrap-prepare | Archive bootstrap's original `variables.less`
+less-bootstrap-concat | Concat bootstrap's `variables.less` with custom variables
+less-bootstrap-compile | Compile customized bootstrap
+less-app | Compile wagman's less
+copy-fonts | Copy fonts into `/public`
+install | Run all tasks above
+
+Bootstrap configuration variables: `/assets/stylesheets/bootstrap.less`
---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+'use strict';
+
+//   Libraries
+
+var fs = require('fs'),
+    gulp = require('gulp'),
+    gulpif = require('gulp-if'),
+    rename = require('gulp-rename'),
+    addsrc = require('gulp-add-src'),
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat');
+
+var argv = require('yargs').argv;
+
+//
+//   Paths and configuration
+//
+var paths = {
+    app : [
+        'assets/javascript/utils.js',
+        'assets/javascript/app.js'
+    ],
+    libs : [
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/bootstrap.min.js',
+        'bower_components/gmaps/gmaps.js'
+    ],
+    ie8 : [
+        'bower_components/html5shiv/dist/html5shiv.min.js',
+        'bower_components/respond-minmax/dest/respond.min.js'
+    ]
+};
+
+//
+//   CSS related tasks
+//
+gulp.task('less-app', function() {
+    return gulp
+        .src('assets/stylesheets/layout.less')
+        .pipe(less())
+        .pipe(gulpif(argv.minify, minifyCSS()))
+        .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('less-bootstrap-prepare', function(cb) {
+    if (!fs.existsSync('bower_components/bootstrap/less/variables.original.less')) {
+        return gulp
+            .src('bower_components/bootstrap/less/variables.less')
+            .pipe(rename('variables.original.less'))
+            .pipe(gulp.dest('bower_components/bootstrap/less'));
+    } else {
+        cb();
+    }
+});
+
+gulp.task('less-bootstrap-concat', ['less-bootstrap-prepare'], function(){
+    return gulp
+        .src('bower_components/bootstrap/less/variables.original.less')
+        .pipe(addsrc.append('assets/stylesheets/bootstrap.less'))
+        .pipe(concat('variables.less'))
+        .pipe(gulp.dest('bower_components/bootstrap/less'))
+});
+
+gulp.task('less-bootstrap-compile', ['less-bootstrap-concat'], function(){
+    return gulp
+        .src('bower_components/bootstrap/less/bootstrap.less')
+        .pipe(less())
+        .pipe(gulpif(argv.minify, minifyCSS()))
+        .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('less-bootstrap', ['less-bootstrap-prepare', 'less-bootstrap-concat', 'less-bootstrap-compile']);
+
+gulp.task('less', ['less-app', 'less-bootstrap']);
+
+//
+//   JS related tasks
+//
+gulp.task('js-app', function() {
+    return gulp
+        .src(paths.app)
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('js-libs', function() {
+    return gulp
+        .src(paths.libs)
+        .pipe(sourcemaps.init())
+        .pipe(concat('vendor.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('js-ie8', function() {
+    return gulp
+        .src(paths.ie8)
+        .pipe(sourcemaps.init())
+        .pipe(concat('ie8.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('js', ['js-app', 'js-libs', 'js-ie8']);
+
+//
+//   Common tasks
+//
+gulp.task('copy-fonts', function() {
+    return gulp
+        .src([
+            'bower_components/bootstrap/fonts/*.*',
+            'bower_components/font-awesome/fonts/*.*'
+        ])
+        .pipe(gulp.dest('public/fonts/'))
+});
+
+//
+//   Entry points
+//
+gulp.task('watch', function () {
+    gulp.watch(['stylesheets/layout.less'], { cwd : "assets" }, ['less-app']);
+    gulp.watch(['stylesheets/bootstrap.less'], { cwd : "assets" }, ['less-bootstrap']);
+
+    gulp.watch(['javascript/**/*.js'], { cwd : "assets" }, ['js']);
+});
+
+
+gulp.task('install', ['copy-fonts', 'js', 'less']);
