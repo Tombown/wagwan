ddfe9daa61f6f62c0efbe7463aebc45a76944486
dmlukichev <dmlukichev@gmail.com>
2015-03-07 03:14:29 +0300

    [dev] Formating event dates

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,72 +1,101 @@
 /**
  * This file contains the common middleware used by your routes.
- * 
+ *
  * Extend or replace these functions as your application requires.
- * 
+ *
  * This structure is not enforced, and just a starting point. If
  * you have more middleware you may want to group it as separate
  * modules in your project's /lib directory.
  */
 
 var _ = require('underscore'),
-	querystring = require('querystring'),
-	keystone = require('keystone');
+    querystring = require('querystring'),
+    keystone = require('keystone'),
+    moment = require('moment');
 
 
 /**
-	Initialises the standard view locals
-	
-	The included layout depends on the navLinks array to generate
-	the navigation in the header, you may wish to change this array
-	or replace it with your own templates / logic.
-*/
+ Initialises the standard view locals
 
-exports.initLocals = function(req, res, next) {
-	
-	var locals = res.locals;
-	
-	locals.navLinks = [
-		{ label: 'Home',		key: 'home',		href: '/' },
-	];
-	
-	locals.user = req.user;
-	
-	next();
-	
-};
-
-
-/**
-	Fetches and clears the flashMessages before a view is rendered
-*/
-
-exports.flashMessages = function(req, res, next) {
-	
-	var flashMessages = {
-		info: req.flash('info'),
-		success: req.flash('success'),
-		warning: req.flash('warning'),
-		error: req.flash('error')
-	};
-	
-	res.locals.messages = _.any(flashMessages, function(msgs) { return msgs.length; }) ? flashMessages : false;
-	
-	next();
-	
-};
-
-
-/**
-	Prevents people from accessing protected pages when they're not signed in
+ The included layout depends on the navLinks array to generate
+ the navigation in the header, you may wish to change this array
+ or replace it with your own templates / logic.
  */
 
-exports.requireUser = function(req, res, next) {
-	
-	if (!req.user) {
-		req.flash('error', 'Please sign in to access this page.');
-		res.redirect('/keystone/signin');
-	} else {
-		next();
-	}
-	
+exports.initLocals = function (req, res, next) {
+    var locals = res.locals;
+
+    locals.navLinks = [
+        {label: 'Home', key: 'home', href: '/'},
+    ];
+
+    locals.user = req.user;
+
+    next();
+};
+
+
+/**
+ Fetches and clears the flashMessages before a view is rendered
+ */
+
+exports.flashMessages = function (req, res, next) {
+    var flashMessages = {
+        info: req.flash('info'),
+        success: req.flash('success'),
+        warning: req.flash('warning'),
+        error: req.flash('error')
+    };
+
+    res.locals.messages = _.any(flashMessages, function (msgs) {
+        return msgs.length;
+    }) ? flashMessages : false;
+
+    next();
+};
+
+
+/**
+ Prevents people from accessing protected pages when they're not signed in
+ */
+
+exports.requireUser = function (req, res, next) {
+    if (!req.user) {
+        req.flash('error', 'Please sign in to access this page.');
+        res.redirect('/keystone/signin');
+    } else {
+        next();
+    }
+};
+
+
+exports.timeFormatter = function(req, res, next) {
+    res.locals.formatTime = function(frm, till) {
+        var now = moment(),
+            start = moment(frm),
+            end = moment(till);
+
+        if (end < now) {
+            return end.fromNow()
+        }
+
+        if (now > start) {
+            return 'now';
+        }
+
+        if (start.diff(now, 'minutes') < 60) {
+            return start.diff(now, 'minutes') + ' minutes';
+        }
+
+        if (start.diff(now, 'hours') < 24) {
+            return start.diff(now, 'hours') + ' hours';
+        }
+
+        if (start.diff(now, 'days') < 2) {
+            return 'tomorrow';
+        }
+
+        return start.format('D MMM');
+    }
+    next();
 };
